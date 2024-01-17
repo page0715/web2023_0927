0396f563605a612d159c9361d05fa0c00f9e5bce
page0715 <page0715@gmail.com>
2024-01-17 13:49:50 +0800

    login

--- s20240117/project/api/user/store.php
+++ s20240117/project/api/user/store.php
@@ -1,5 +1,14 @@
 <?php 
-$data = $_POST;
+// $data = $_POST;
+
+$data = [
+    'acc' => 'admin',
+    'pwd' => '12345'
+];
+$sql = "SELECT * FROM `user` WHERE acc = 'admin'";
+
+
+//
 
 // php
 $url = header('Location: .....');
