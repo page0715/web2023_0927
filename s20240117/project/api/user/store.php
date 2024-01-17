8eb3f4eeae17fe0fac0b2e52bf20e859139673b1
page0715 <page0715@gmail.com>
2024-01-17 13:08:56 +0800

    crud views project

---
+++ s20240117/project/api/user/store.php
@@ -0,0 +1,8 @@
+<?php 
+$data = $_POST;
+
+// php
+$url = header('Location: .....');
+
+// sql = "insert into table column value cloumn"....
+?>
---
+++ s20240117/project/api/user/update.php
@@ -0,0 +1,5 @@
+<?php 
+$data = $_POST;
+
+// sql = "UPDATE table SET column WHERE condition id = 1 "....
+?>
