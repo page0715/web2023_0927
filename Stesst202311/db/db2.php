6bf858ef86703a36f76f29d541610440f9ebbd30
page0715 <page0715@gmail.com>
2024-01-29 16:36:56 +0800

    sum

---
+++ Stesst202311/db/db2.php
@@ -0,0 +1,26 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+class DB{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db15";
+    protected $pdo;
+    protected $table;
+
+
+    public 
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+?>
